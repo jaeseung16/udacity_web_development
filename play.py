7e9e88d7c96815d658ed2f4654027e542930d1e0
Jae-Seung Lee <jaeseung@gmail.com>
2016-10-08 20:48:24 -0400

    Update play.py

--- play.py
+++ play.py
@@ -1,15 +1,20 @@
 import webapp2
 form="""
 <html>
-<form action="http://www.google.com/search">
+<form action="/testform">
         <input name="q">
         <input type="submit">
 </form>
 </html>
 """
 class MainPage(webapp2.RequestHandler):
-  def get(self):
-    #self.response.headers['Content-Type'] = 'text/plain'
-    self.response.out.write(form)
+        def get(self):
+                #self.response.headers['Content-Type'] = 'text/plain'
+                self.response.out.write(form)
+        
+class TestHandler(webapp2.RequestHandler):
+        def get(self):
+                q = self.request.get("q")
+                self.response.out.write(q)
     
 app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
